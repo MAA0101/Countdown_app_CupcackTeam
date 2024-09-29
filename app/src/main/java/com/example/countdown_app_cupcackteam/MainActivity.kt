375239b58761370d9e1a19acc550f21571a95395
Hunterz243 <118853488+MAA0101@users.noreply.github.com>
2024-09-29 21:41:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/countdown_app_cupcackteam/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.countdown_app_cupcackteam
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.countdown_app_cupcackteam.ui.theme.Countdown_app_CupcackTeamTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Countdown_app_CupcackTeamTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Countdown_app_CupcackTeamTheme {
+        Greeting("Android")
+    }
+}
